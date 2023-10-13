0345227d8b84abb21f8d7ff5e7772c52af340b69
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2023-10-14 00:07:21 +0300

    build: configure alias imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': `${path.resolve(__dirname, "./src/assets/")}`,
+      '@components': `${path.resolve(__dirname, "./src/components")}`,
+      '@data': `${path.resolve(__dirname, "./src/data")}`,
+      '@helpers': `${path.resolve(__dirname, "./src/helpers")}`,
+      '@hooks': `${path.resolve(__dirname, "./src/hooks")}`,
+      '@pages': `${path.resolve(__dirname, "./src/pages")}`,
+      '@store': `${path.resolve(__dirname, "./src/store")}`,
+    },
+  },
+});
